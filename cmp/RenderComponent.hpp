fa94731c395a535730fe0a9dbd01ac499a145514
isaigm <isaigm23@gmail.com>
2025-05-28 20:27:22 -0600

    removing fsanitazer flag

--- cmp/RenderComponent.hpp
+++ cmp/RenderComponent.hpp
@@ -2,10 +2,9 @@
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <memory>
+#include <iostream>
 struct RenderComponent
 {
-	//RenderComponent(const sf::Texture &texture) : sprite(std::make_shared<sf::Sprite>(texture)) {}
 	RenderComponent(){}
 	std::shared_ptr<sf::Sprite> sprite;
-
 };
